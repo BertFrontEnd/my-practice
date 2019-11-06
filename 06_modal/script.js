d07630ff536be77ded264c4f35236411da178099
BertFrontEnd <Berton@yandex.ru>
2019-11-06 23:56:10 +0300

    Added Modal Window creation

---
+++ 06_modal/script.js
@@ -0,0 +1,23 @@
+const myModal = document.querySelector('#myModal');
+console.log(myModal);
+const myBtn = document.querySelector('#myBtn');
+console.log(myBtn);
+const close = document.querySelectorAll('.close')[0];
+console.log(close);
+
+myBtn.onclick = function() {
+  myModal.style.display = 'block';
+  console.log(myModal);
+};
+
+close.onclick = function() {
+  myModal.style.display = 'none';
+  console.log(myModal);
+};
+
+window.onclick = function(e) {
+  if (e.target == myModal) {
+    myModal.style.display = "none";
+    console.log(myModal);
+  }
+};
