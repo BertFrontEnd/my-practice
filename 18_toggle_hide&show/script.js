aa028df57c8a04ed5e7daad038cfc12a4990406f
BertFrontEnd <Berton@yandex.ru>
2019-12-03 23:21:39 +0300

    Updated Toggle 'Hide' & 'Show'

--- 18_toggle_hide&show/script.js
+++ 18_toggle_hide&show/script.js
@@ -1,6 +1,6 @@
-const btn = document.querySelector('button');
+const btnText = document.querySelector('#text');
 
-btn.onclick = function() {
+btnText.onclick = function() {
   let tog = document.querySelector('#mySection');
   if (tog.style.display === 'none') {
     tog.style.display = 'block';
@@ -8,3 +8,26 @@ btn.onclick = function() {
     tog.style.display = 'none';
   }
 };
+
+const btnAlerts = document.querySelector('#alerts');
+
+btnAlerts.onclick = function() {
+  let tog = document.querySelector('#myAlerts');
+  if (tog.style.display === 'none') {
+    tog.style.display = 'block';
+  } else {
+    tog.style.display = 'none';
+  }
+};
+
+const close = document.querySelectorAll('.alert__close--btn');
+
+for (let i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    let elem = this.parentNode;
+    elem.style.opacity = '0';
+    setTimeout(function() {
+      elem.style.display = 'none';
+    }, 600);
+  };
+}
